3a928e3e580b28a30f13ef3f31dac941aaa906c4
Robert-Rodriguez <robertmasters@hotmail.com>
2020-07-15 11:04:25 -0700

    finished task 2, going to lunch

--- index.js
+++ index.js
@@ -40,11 +40,16 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
-
-    /*code here*/
-
+function is31Flavors(arraysMan){
+    if(arraysMan.length ==31){
+      return true
+    } else {
+    
+    return false 
+    }
 }
+
+console.log(is31Flavors(originalFlavors));
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -57,11 +62,15 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
-
+function addFlavor(arr, newFlavor){
+    arr.unshift(newFlavor);
     /*code here*/
 
 }
+addFlavor(originalFlavors,"Rainbow Sherbert")
+
+
+console.log(originalFlavors)
 
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
